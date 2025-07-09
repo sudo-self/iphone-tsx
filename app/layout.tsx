43de9a68cd75ddf6e68cbfe2ba0bc1d4e599631a
sudo-self <sudo-self@proton.me>
2025-07-09 13:52:10 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'iphone tsx',
+  description: 'A mock phone gui with supabase and redis initigration',
   generator: 'v0.dev',
 }
 
