9b9025a1c309f61f3e240491809280eb9b7a8aa3
sudo-self <sudo-self@proton.me>
2025-07-09 15:53:37 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: 'iphone tsx',
-  description: 'A mock phone gui with supabase and redis initigration',
-  generator: 'v0.dev',
+  title: "iphone tsx",
+  description: "A mock phone gui with supabase and redis integration",
+  generator: "v0.dev",
 }
 
 export default function RootLayout({
@@ -13,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-full">
+      <body className="h-full">
+        <div className="h-full">{children}</div>
+      </body>
     </html>
   )
 }
+
