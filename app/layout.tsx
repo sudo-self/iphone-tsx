27c5e51edb1853cf6726d6b4741890c973dde712
sudo-self <tech@jessejesse.com>
2025-07-12 00:02:13 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,23 +55,6 @@ export default function RootLayout({
   );
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className="h-full">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body className="h-full">
-        <ClientSessionProvider>{children}</ClientSessionProvider>
-      </body>
-    </html>
-  );
-}
-
 
 
 
